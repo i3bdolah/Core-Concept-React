eec942524043f8764b485f9cc181256d46c86b73
Abdullah Ban <xbdullahx@gmail.com>
2023-04-19 21:54:25 +0300

    Replacing useState with useRef.

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,26 +1,30 @@
 import React from 'react'
 import Button from './UI/Button'
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import Alert from './Alert'
 import Wrapper from './Helpers/Wrapper'
 
 const Form = (props) => {
-  const [username, setUsername] = useState('')
-  const [age, setAge] = useState('')
+  const userInputRef = useRef()
+  const ageInputRef = useRef()
+
+  // const [username, setUsername] = useState('')
+  // const [age, setAge] = useState('')
   const [isValid, setIsValid] = useState(true)
   const [msg, setMsg] = useState('')
 
-  const onChangeUsername = (e) => {
-    setUsername(e.target.value)
-  }
+  // const onChangeUsername = (e) => {
+  //   setUsername(e.target.value)
+  // }
 
-  const onChangeAge = (e) => {
-    setAge(e.target.value)
-  }
+  // const onChangeAge = (e) => {
+  //   setAge(e.target.value)
+  // }
 
   const onSubmitUser = (e) => {
     e.preventDefault()
-    console.log(username, age)
+    const username = userInputRef.current.value
+    const age = ageInputRef.current.value
 
     if (
       username.includes(' ') ||
@@ -31,7 +35,13 @@ const Form = (props) => {
       setMsg('Username field: Enter Valid Username.')
       return
     }
-    if (age > 300 || age < 0 || age.includes(' ') || age.trim().length === 0) {
+    if (
+      age > 300 ||
+      age < 0 ||
+      age.includes(' ') ||
+      age.trim().length === 0 ||
+      isNaN(age)
+    ) {
       setIsValid(false)
       setMsg('Age field: Enter Valid Age.')
       return
@@ -43,6 +53,8 @@ const Form = (props) => {
     }
     props.onGetData(obj)
     setIsValid(true)
+    userInputRef.current.value = ''
+    ageInputRef.current.value = ''
   }
 
   const closeAlertHandler = () => {
@@ -58,15 +70,17 @@ const Form = (props) => {
           <div>
             <label className="m-3 font-medium">Username</label>
             <input
+              ref={userInputRef}
               type="text"
-              className="w-[300px] h-[30px] m-3 block"
-              onChange={onChangeUsername}
+              className="w-[300px] h-[30px] m-3 block p-2 rounded-lg"
+              // onChange={onChangeUsername}
             />
             <label className="m-3 font-medium">Age (Years)</label>
             <input
+              ref={ageInputRef}
               type="text"
-              className="w-[300px] h-[30px] m-3 block"
-              onChange={onChangeAge}
+              className="w-[300px] h-[30px] m-3 block p-2 rounded-lg"
+              // onChange={onChangeAge}
             />
           </div>
           <div className="flex justify-center">
